1b86e79b0f87f202ace25795cf3b143aed6ac238
Soyuz <sgopg062024@gmail.com>
2026-04-12 13:27:41 -0400

    New branch created and all files copied from shell_assignment1

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -26,6 +26,7 @@ unzip -q rawdata.zip
 
 ###########################################
 # Complete assignment here
+# Also contributed by Soyuz Gautam, 4/12/2026
 
 # 1. Create a directory named data
 
